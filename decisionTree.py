4383f40706b20e27f86b427540ca5b322fc71930
kedumuc1712 <sieutroma7k51@gmail.com>
2019-05-08 14:54:31 +0700

    Change decisionTree by algorithm ID3

--- decisionTree.py
+++ decisionTree.py
@@ -1,172 +1,126 @@
-# CART on the Bank Note dataset
-from random import seed
-from random import randrange
-from csv import reader
- 
-# Load a CSV file
-def load_csv(filename):
-	file = open(filename, "rt")
-	lines = reader(file)
-	dataset = list(lines)
-	return dataset
- 
-# Convert string column to float
-def str_column_to_float(dataset, column):
-	for row in dataset:
-		row[column] = float(row[column].strip())
- 
-# Split a dataset into k folds
-def cross_validation_split(dataset, n_folds):
-	dataset_split = list()
-	dataset_copy = list(dataset)
-	fold_size = int(len(dataset) / n_folds)
-	for i in range(n_folds):
-		fold = list()
-		while len(fold) < fold_size:
-			index = randrange(len(dataset_copy))
-			fold.append(dataset_copy.pop(index))
-		dataset_split.append(fold)
-	return dataset_split
- 
-# Calculate accuracy percentage
-def accuracy_metric(actual, predicted):
-	correct = 0
-	for i in range(len(actual)):
-		if actual[i] == predicted[i]:
-			correct += 1
-	return correct / float(len(actual)) * 100.0
- 
-# Evaluate an algorithm using a cross validation split
-def evaluate_algorithm(dataset, algorithm, n_folds, *args):
-	folds = cross_validation_split(dataset, n_folds)
-	scores = list()
-	for fold in folds:
-		train_set = list(folds)
-		train_set.remove(fold)
-		train_set = sum(train_set, [])
-		test_set = list()
-		for row in fold:
-			row_copy = list(row)
-			test_set.append(row_copy)
-			row_copy[-1] = None
-		predicted = algorithm(train_set, test_set, *args)
-		actual = [row[-1] for row in fold]
-		accuracy = accuracy_metric(actual, predicted)
-		scores.append(accuracy)
-	return scores
- 
-# Split a dataset based on an attribute and an attribute value
-def test_split(index, value, dataset):
-	left, right = list(), list()
-	for row in dataset:
-		if row[index] < value:
-			left.append(row)
-		else:
-			right.append(row)
-	return left, right
- 
-# Calculate the Gini index for a split dataset
-def gini_index(groups, classes):
-	# count all samples at split point
-	n_instances = float(sum([len(group) for group in groups]))
-	# sum weighted Gini index for each group
-	gini = 0.0
-	for group in groups:
-		size = float(len(group))
-		# avoid divide by zero
-		if size == 0:
-			continue
-		score = 0.0
-		# score the group based on the score for each class
-		for class_val in classes:
-			p = [row[-1] for row in group].count(class_val) / size
-			score += p * p
-		# weight the group score by its relative size
-		gini += (1.0 - score) * (size / n_instances)
-	return gini
- 
-# Select the best split point for a dataset
-def get_split(dataset):
-	class_values = list(set(row[-1] for row in dataset))
-	b_index, b_value, b_score, b_groups = 999, 999, 999, None
-	for index in range(len(dataset[0])-1):
-		for row in dataset:
-			groups = test_split(index, row[index], dataset)
-			gini = gini_index(groups, class_values)
-			if gini < b_score:
-				b_index, b_value, b_score, b_groups = index, row[index], gini, groups
-	return {'index':b_index, 'value':b_value, 'groups':b_groups}
- 
-# Create a terminal node value
-def to_terminal(group):
-	outcomes = [row[-1] for row in group]
-	return max(set(outcomes), key=outcomes.count)
- 
-# Create child splits for a node or make terminal
-def split(node, max_depth, min_size, depth):
-	left, right = node['groups']
-	del(node['groups'])
-	# check for a no split
-	if not left or not right:
-		node['left'] = node['right'] = to_terminal(left + right)
-		return
-	# check for max depth
-	if depth >= max_depth:
-		node['left'], node['right'] = to_terminal(left), to_terminal(right)
-		return
-	# process left child
-	if len(left) <= min_size:
-		node['left'] = to_terminal(left)
-	else:
-		node['left'] = get_split(left)
-		split(node['left'], max_depth, min_size, depth+1)
-	# process right child
-	if len(right) <= min_size:
-		node['right'] = to_terminal(right)
-	else:
-		node['right'] = get_split(right)
-		split(node['right'], max_depth, min_size, depth+1)
- 
-# Build a decision tree
-def build_tree(train, max_depth, min_size):
-	root = get_split(train)
-	split(root, max_depth, min_size, 1)
-	return root
- 
-# Make a prediction with a decision tree
-def predict(node, row):
-	if row[node['index']] < node['value']:
-		if isinstance(node['left'], dict):
-			return predict(node['left'], row)
-		else:
-			return node['left']
-	else:
-		if isinstance(node['right'], dict):
-			return predict(node['right'], row)
-		else:
-			return node['right']
- 
-# Classification and Regression Tree Algorithm
-def decision_tree(train, test, max_depth, min_size):
-	tree = build_tree(train, max_depth, min_size)
-	predictions = list()
-	for row in test:
-		prediction = predict(tree, row)
-		predictions.append(prediction)
-	return(predictions)
- 
-# Test CART on Bank Note dataset
-seed(1)
-# load and prepare data
-filename = 'data_banknote_authentication.txt'
-dataset = load_csv(filename)
-# convert string attributes to integers
-for i in range(len(dataset[0])):
-	str_column_to_float(dataset, i)
-# evaluate algorithm
-n_folds = 5
-max_depth = 5
-min_size = 10
-scores = evaluate_algorithm(dataset, decision_tree, n_folds, max_depth, min_size)
-print('Scores: %s' % scores)
-print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
+from __future__ import print_function 
+import numpy as np 
+import pandas as pd 
+
+
+class TreeNode(object):
+    def __init__(self, ids = None, children = [], entropy = 0, depth = 0):
+        self.ids = ids           # index of data in this node
+        self.entropy = entropy   # entropy, will fill later
+        self.depth = depth       # distance to root node
+        self.split_attribute = None # which attribute is chosen, it non-leaf
+        self.children = children # list of its child nodes
+        self.order = None       # order of values of split_attribute in children
+        self.label = None       # label of node if it is a leaf
+
+    def set_properties(self, split_attribute, order):
+        self.split_attribute = split_attribute
+        self.order = order
+
+    def set_label(self, label):
+        self.label = label
+
+
+def entropy(freq):
+    # remove prob 0 
+    freq_0 = freq[np.array(freq).nonzero()[0]]
+    prob_0 = freq_0/float(freq_0.sum())
+    return -np.sum(prob_0*np.log(prob_0))
+
+class DecisionTreeID3(object):
+    def __init__(self, max_depth= 10, min_samples_split = 2, min_gain = 1e-4):
+        self.root = None
+        self.max_depth = max_depth 
+        self.min_samples_split = min_samples_split 
+        self.Ntrain = 0
+        self.min_gain = min_gain
+    
+    def fit(self, data, target):
+        self.Ntrain = data.count()[0]
+        self.data = data 
+        self.attributes = list(data)
+        self.target = target 
+        self.labels = target.unique()
+        
+        ids = range(self.Ntrain)
+        self.root = TreeNode(ids = ids, entropy = self._entropy(ids), depth = 0)
+        queue = [self.root]
+        while queue:
+            node = queue.pop()
+            if node.depth < self.max_depth or node.entropy < self.min_gain:
+                node.children = self._split(node)
+                if not node.children: #leaf node
+                    self._set_label(node)
+                queue += node.children
+            else:
+                self._set_label(node)
+                
+    def _entropy(self, ids):
+        # calculate entropy of a node with index ids
+        if len(ids) == 0: return 0
+        ids = [i+1 for i in ids] # panda series index starts from 1
+        freq = np.array(self.target[ids].value_counts())
+        return entropy(freq)
+
+    def _set_label(self, node):
+        # find label for a node if it is a leaf
+        # simply chose by major voting 
+        target_ids = [i + 1 for i in node.ids]  # target is a series variable
+        node.set_label(self.target[target_ids].mode()[0]) # most frequent label
+    
+    def _split(self, node):
+        ids = node.ids 
+        best_gain = 0
+        best_splits = []
+        best_attribute = None
+        order = None
+        sub_data = self.data.iloc[ids, :]
+        for i, att in enumerate(self.attributes):
+            values = self.data.iloc[ids, i].unique().tolist()
+            if len(values) == 1: continue # entropy = 0
+            splits = []
+            for val in values: 
+                sub_ids = sub_data.index[sub_data[att] == val].tolist()
+                splits.append([sub_id-1 for sub_id in sub_ids])
+            # don't split if a node has too small number of points
+            if min(map(len, splits)) < self.min_samples_split: continue
+            # information gain
+            HxS= 0
+            for split in splits:
+                HxS += len(split)*self._entropy(split)/len(ids)
+            gain = node.entropy - HxS 
+            if gain < self.min_gain: continue # stop if small gain 
+            if gain > best_gain:
+                best_gain = gain 
+                best_splits = splits
+                best_attribute = att
+                order = values
+        node.set_properties(best_attribute, order)
+        child_nodes = [TreeNode(ids = split,
+                     entropy = self._entropy(split), depth = node.depth + 1) for split in best_splits]
+        return child_nodes
+
+    def predict(self, new_data):
+        """
+        :param new_data: a new dataframe, each row is a datapoint
+        :return: predicted labels for each row
+        """
+        npoints = new_data.count()[0]
+        labels = [None]*npoints
+        for n in range(npoints):
+            x = new_data.iloc[n, :] # one point 
+            # start from root and recursively travel if not meet a leaf 
+            node = self.root
+            while node.children: 
+                node = node.children[node.order.index(x[node.split_attribute])]
+            labels[n] = node.label
+            
+        return labels
+
+if __name__ == "__main__":
+    df = pd.DataFrame.from_csv('weather.csv')
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+    tree = DecisionTreeID3(max_depth = 3, min_samples_split = 2)
+    tree.fit(X, y)
+    print(tree.predict(X))
